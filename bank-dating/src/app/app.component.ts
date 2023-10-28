2ea7cbfca7c1b394ee8e42fc05855fd9e5ecea2d
Noel <noel.marjanovic@pax.ch>
2023-10-28 20:04:50 +0200

    Inserted mock data for bank-dating

--- bank-dating/src/app/app.component.ts
+++ bank-dating/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {User} from './models/user.interface';
+import {Account} from './models/account.interface';
+import {Transaction} from './models/transaction.interface';
+import {formatDate} from './util/date.util';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,77 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bank-dating';
+
+  user: User = {} as User;
+  account: Account = {} as Account;
+  constructor() {
+    this.mockValuesFromDevPortal();
+  }
+
+  requestValuesFromDevPortal() {}
+  mockValuesFromDevPortal() {
+    this.account = {
+      balance: 23000,
+      transactions: [
+        {
+          timeStamp: formatDate(new Date()),
+          amount: 275,
+          description: 'Zusatz Krankenkasse',
+          location: 'Assura',
+          standingOrder: true,
+        } as Transaction,
+        {
+          timeStamp: formatDate(new Date()),
+          amount: 200,
+          description: 'Krankenkasse',
+          location: 'Atupri',
+          standingOrder: true,
+        } as Transaction,
+        {
+          timeStamp: formatDate(new Date()),
+          amount: 900,
+          description: 'Drone',
+          location: 'Media Markt',
+          standingOrder: false,
+        } as Transaction,
+        {
+          timeStamp: formatDate(new Date()),
+          amount: 302,
+          description: 'Drone',
+          location: 'Media Markt',
+          standingOrder: false,
+        } as Transaction,
+        {
+          timeStamp: formatDate(new Date()),
+          amount: 500,
+          description: 'Drone',
+          location: 'Media Markt',
+          standingOrder: false,
+        } as Transaction,
+        {
+          timeStamp: formatDate(new Date()),
+          amount: 700,
+          description: 'Drone',
+          location: 'Media Markt',
+          standingOrder: false,
+        } as Transaction,
+        {
+          timeStamp: formatDate(new Date()),
+          amount: 700,
+          description: 'Drone',
+          location: 'Media Markt',
+          standingOrder: false,
+        } as Transaction,
+      ],
+    } as Account;
+
+    this.user = {
+      userId: '1',
+      firstname: 'Esra',
+      lastname: 'Doerksen',
+      birthdate: new Date('2002-08-05'),
+      city: 'Basel',
+      sex: 'Männlich',
+    } as User;
+  }
 }
--- bank-dating/src/app/chat-list/chat-list.component.ts
+++ bank-dating/src/app/chat-list/chat-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {User} from '../models/user.interface';
 
 @Component({
   selector: 'app-chat-list',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./chat-list.component.css']
 })
 export class ChatListComponent {
-
+  @Input() user: User = {} as User;
 }
