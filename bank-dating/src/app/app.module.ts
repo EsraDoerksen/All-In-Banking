3728417ca5a76fe52b223e9b43191cfcc0ff9fbc
Noel <noel.marjanovic@pax.ch>
2023-10-28 14:16:35 +0200

    Implemented raw frontend

--- bank-dating/src/app/app.module.ts
+++ bank-dating/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonCardComponent } from './person-card/person-card.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { MatchesComponent } from './matches/matches.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonCardComponent,
+    SideNavComponent,
+    MatchesComponent,
+    TopNavComponent
   ],
   imports: [
     BrowserModule,
