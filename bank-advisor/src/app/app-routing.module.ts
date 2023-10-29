aa9749952edb138a8da1acb4d914bcc7873664e9
EsraDoerksen <esra.doerksen@pax.ch>
2023-10-29 10:24:42 +0100

    adds styling and last routing

--- bank-advisor/src/app/app-routing.module.ts
+++ bank-advisor/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { BigExpensesComponent } from './components/big-expenses/big-expenses.component';
 import { RegularExpensesComponent } from './components/regular-expenses/regular-expenses.component';
 import { AllExpensesComponent } from './components/all-expenses/all-expenses.component';
+import { ContractExpensesComponent } from './components/contract-expenses/contract-expenses.component';
 
 const routes: Routes = [
   { path: '', component: AllExpensesComponent },
   { path: 'big-expenses', component: BigExpensesComponent },
   { path: 'regular-expenses', component: RegularExpensesComponent },
+  { path: 'contracts', component: ContractExpensesComponent },
   { path: '**', redirectTo: 'matches', pathMatch: 'full' },
 ];
 
