8990768d2575663be7ce30889960375ce163137c
EsraDoerksen <esra.doerksen@pax.ch>
2023-10-29 10:11:37 +0100

    adds routing and styling

--- bank-advisor/src/app/components/contract-expenses/contract-expenses.component.ts
+++ bank-advisor/src/app/components/contract-expenses/contract-expenses.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Transaction } from 'src/app/models/transaction.interface';
+import { User } from '../../models/user.interface';
+import { Account } from '../../models/account.interface';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-contract-expenses',
@@ -7,5 +10,15 @@ import { Transaction } from 'src/app/models/transaction.interface';
   styleUrls: ['./contract-expenses.component.css'],
 })
 export class ContractExpensesComponent {
-  @Input() contractTransactions: Transaction[] = [];
+  private user: User = {} as User;
+  private account: Account = {} as Account;
+  contractTransactions: Transaction[] = [];
+
+  constructor(dataService: DataService) {
+    this.user = dataService.getUser();
+    this.account = dataService.getAccount();
+    this.contractTransactions = this.account.transactions.filter(
+      (t) => t.standingOrder
+    );
+  }
 }
--- bank-advisor/src/app/components/regular-expenses/regular-expenses.component.ts
+++ bank-advisor/src/app/components/regular-expenses/regular-expenses.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Transaction } from 'src/app/models/transaction.interface';
+import { DataService } from '../../services/data.service';
+import { Account } from '../../models/account.interface';
+import { User } from '../../models/user.interface';
 
 @Component({
   selector: 'app-regular-expenses',
@@ -7,5 +10,25 @@ import { Transaction } from 'src/app/models/transaction.interface';
   styleUrls: ['./regular-expenses.component.css'],
 })
 export class RegularExpensesComponent {
-  @Input() regularTransactions: Transaction[] = [];
+  private user: User = {} as User;
+  private account: Account = {} as Account;
+  regularTransactions: Transaction[] = [];
+
+  constructor(dataService: DataService) {
+    this.user = dataService.getUser();
+    this.account = dataService.getAccount();
+    const transactionCounts: { [key: string]: number } = {};
+
+    this.account.transactions.forEach((t) => {
+      if (transactionCounts[t.location]) {
+        transactionCounts[t.location]++;
+      } else {
+        transactionCounts[t.location] = 1;
+      }
+    });
+
+    this.regularTransactions = this.account.transactions.filter(
+      (t) => transactionCounts[t.location] > 2
+    );
+  }
 }
