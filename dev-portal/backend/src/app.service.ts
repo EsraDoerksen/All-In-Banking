a987559cb838d39f0149442f5cd009d7d2de2f9d
Benjamin <benjamin.bach2@pax.ch>
2023-10-29 12:32:55 +0100

    iframe

--- dev-portal/backend/src/app.service.ts
+++ dev-portal/backend/src/app.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
 import { App } from './entity/app.entity';
+import { SharedDb } from './entity/shared-db.entity';
 
 @Injectable()
 export class AppService {
   constructor(
     @InjectRepository(App)
     private readonly appRepository: Repository<App>,
+    @InjectRepository(SharedDb)
+    private readonly sharedDbRepository: Repository<SharedDb>,
   ) {}
 
   async listApps() {
@@ -16,5 +19,17 @@ export class AppService {
       ...app,
       url: `http://localhost:3001?${app.appId}`,
     }));
+  }
+
+  saveSharedDb(appId: string, key: string, value: any) {
+    this.sharedDbRepository.upsert({ key: appId + key, value }, ['key']);
+  }
+
+  listSharedDb(appId: string) {
+    return this.sharedDbRepository.find({
+      where: {
+        key: Like(`${appId}%`),
+      },
+    });
   }
 }
